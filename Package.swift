bd7ebb886ff7a9a429d1ea368e13affdbac699af
ganesh-o18 <142499462+ganesh-o18@users.noreply.github.com>
2026-05-11 11:36:42 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Offer18SDK",
-            url: "https://github.com/user-attachments/files/27582054/Offer18SDK.xcframework.zip",
+            url: "https://github.com/ganesh-o18/o18isdk/releases/download/0.0.1/Offer18SDK.xcframework.zip",
             checksum: "f52f6a5f9e1a94f6e1848968a91a0b44128872e42c27fc7eeb9c0ff273a4962b"
         )
     ]
